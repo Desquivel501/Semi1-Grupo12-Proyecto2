c946eb92a9661aec4859eff6b502bc56b435413e
DanielAcabal <danchiacabal@gmail.com>
2023-10-10 19:20:53 -0600

    Added s3

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -2,13 +2,14 @@ import express from "express";
 import cors from "cors";
 import "dotenv/config";
 import { authRouter } from "./routes/auth";
+import { userRouter } from "./routes/user";
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
 app.use("/api", authRouter);
-//app.use("/api/users", userRouter);
+app.use("/api/users", userRouter);
 //app.use("/api/posts", songRouter);
 
 app.get("/", (req, res) => {
--- backend/src/customTypes/types.ts
+++ backend/src/customTypes/types.ts
@@ -3,3 +3,10 @@ export type Credentials = {
   password: string;
   photo?: string;
 };
+export type User = {
+  name: string
+  email: string
+  dpi: number
+  password:string
+  avatar: string
+}
