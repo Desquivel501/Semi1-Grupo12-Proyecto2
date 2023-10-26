23e619b8749582f0afcb39315b33b69471da9644
DanielAcabal <danchiacabal@gmail.com>
2023-10-26 01:30:21 -0600

    Added chatbot

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -2,44 +2,58 @@ const API = import.meta.env.VITE_API;
 import { getSession } from "../auth/auth";
 
 export function registrar(data) {
-    return fetch(`${API}/users/create`, {
-      method: "POST",
-      body: data,
-      headers: {
-        "Access-Control-Allow-Origin": "*",
-      },
-    })
+  return fetch(`${API}/users/create`, {
+    method: "POST",
+    body: data,
+    headers: {
+      "Access-Control-Allow-Origin": "*",
+    },
+  })
     .then((res) => res.json())
     .catch((err) => console.log(err));
 }
 
 export async function sendFormData({ endpoint, data }) {
-    
-    const session = await getSession();
-    const idToken = session.idToken
+  const session = await getSession();
+  const idToken = session.idToken;
 
-    return fetch(`${API}${endpoint}`, {
-        method: "POST",
-        body: data,
-        headers: {
-            Authorization: `Bearer ${idToken.jwtToken}`,
-            "Access-Control-Allow-Origin_Origin": "*",
-        },
-    })
+  return fetch(`${API}${endpoint}`, {
+    method: "POST",
+    body: data,
+    headers: {
+      Authorization: `Bearer ${idToken.jwtToken}`,
+      "Access-Control-Allow-Origin_Origin": "*",
+    },
+  })
     .then((res) => res.json())
     .catch((err) => console.log(err));
 }
 
 export async function getData({ endpoint }) {
-
   const session = await getSession();
-  const idToken = session.idToken
+  const idToken = session.idToken;
 
-  return fetch(`${API}${endpoint}`,{
+  return fetch(`${API}${endpoint}`, {
     headers: {
-        Authorization: `Bearer ${idToken.jwtToken}`,
+      Authorization: `Bearer ${idToken.jwtToken}`,
     },
   })
     .then((res) => res.json())
     .catch((er) => console.log(er));
+}
+
+export async function sendJsonData({ endpoint, data }) {
+  const session = await getSession();
+  const idToken = session.idToken;
+  return fetch(`${API}${endpoint}`, {
+    method: "POST",
+    body: JSON.stringify(data),
+    headers: {
+      Authorization: `Bearer ${idToken.jwtToken}`,
+      "Access-Control-Allow-Origin_Origin": "*",
+      "Content-Type": "application/json",
+    },
+  })
+  .then((res) => res.json())
+  .catch((err) => console.log(err));
 }
--- frontend/src/pages/ChatBot/ChatBot.jsx
+++ frontend/src/pages/ChatBot/ChatBot.jsx
@@ -1,69 +1,89 @@
-import { 
-    Grid,
-    Box,
-} from '@mui/material';
+import { Box, Grid } from "@mui/material";
 
+import { useNavigate } from "react-router-dom";
+import { useContext, useEffect, useState } from "react";
+import { getCurrentUser, getSession } from "../../auth/auth";
 
-import { useNavigate } from 'react-router-dom';
-import { useContext, useEffect, useState } from 'react';
-import { getSession } from '../../auth/auth';
-
-import ChatContainerBot from '../../components/ChatContainerBot/ChatContainerBot';
+import ChatContainerBot from "../../components/ChatContainerBot/ChatContainerBot";
+import ChatContainer from "../../components/ChatContainer/ChatContainer";
+import { sendJsonData } from "../../api/api";
 
 function ChatBot() {
+  const navigate = useNavigate();
+  const [user, setUser] = useState(null);
+  const [conversation, setConversation] = useState([]);
+  const botInfo = {
+    image:
+      "https://t4.ftcdn.net/jpg/03/51/61/49/360_F_351614912_nhPej8tYdn8gytfBnBPag8HBUt2vaznE.jpg",
+    name: "USAC",
+    family_name: "Bot",
+  };
 
-    const navigate = useNavigate();
-    const [user, setUser] = useState(null)
-
-    useEffect(() => {
-        const getCurrentUser = async () => {
-        try {
-            const user = await getSession()
-            setUser(user)
-            if (user === null) {
-                navigate('/')
-            }
-        } catch (err) {
-            // not logged in
-            console.log(err)
-            setUser(null)
-            navigate('/')
+  useEffect(() => {
+    const start = async () => {
+      try {
+        const user = await getCurrentUser();
+        if (user === null) {
+          navigate("/");
         }
-        }
-        getCurrentUser()
-    }, [])
+        setUser(user);
+        console.log(user);
+      } catch (err) {
+        // not logged in
+        console.log(err);
+        setUser(null);
+        navigate("/");
+      }
+    };
+    start();
+  }, []);
+  const handleSubmit = (text) => {
+    const endpoint = "/chatBot/send";
+    const data = {
+      email: user.email,
+      text,
+    };
+    sendJsonData({ endpoint, data })
+      .then((messages) => {
+        const prevConversation = [...conversation]
+        const updatedConversation = prevConversation.concat(messages)
+        setConversation(updatedConversation) 
+      });
+  };
+  return (
+    <>
+      <Box
+        component="main"
+        display="flex"
+        width="45vw"
+        height="80vh"
+        sx={{
+          flexGrow: 1,
+          border: 0,
+          borderColor: "primary.main",
+          mt: "70px",
+        }}
+        justifyContent="center"
+      >
+        <Grid
+          container
+          spacing={3}
+          sx={{ width: "100%" }}
+          alignItems="top"
+          justifyContent="center"
+        >
+          {/* <ChatContainerBot />*/}
 
-    return (
-        <>
-            <Box
-                component="main"
-                display="flex"
-                width="45vw"
-                height="80vh"
-                sx={{
-                    flexGrow: 1,
-                    border: 0,
-                    borderColor: 'primary.main',
-                    mt:'70px',
-                }}
-                justifyContent='center'
-            >
-                
-                <Grid
-                    container
-                    spacing={3}
-                    sx={{ width: "100%"}}
-                    alignItems="top"
-                    justifyContent="center"
-                >
-
-                    <ChatContainerBot />
-                
-                </Grid>
-
-            </Box>
-        </>
-    )
+          <ChatContainer
+            actualUser={user}
+            friend={botInfo}
+            conversation={conversation}
+            onSubmit={handleSubmit}
+          />
+        </Grid>
+      </Box>
+    </>
+  );
 }
 
-export default ChatBot
+export default ChatBot;
