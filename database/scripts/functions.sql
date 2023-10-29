d2d9fc1fced0a68e50ad571db89695b04c21edb7
José Montenegro <3013297960101@ingenieria.usac.edu.gt>
2023-10-28 19:31:36 -0600

    Agregando procedimientos para manejo de amigos (recepción y envío de solicitudes, aceptar o rechazar estas y ver amigos y no amigos)

--- database/scripts/functions.sql
+++ database/scripts/functions.sql
@@ -33,3 +33,94 @@ BEGIN
     SELECT EXISTS( SELECT 1 FROM Users u WHERE u.email = email) INTO existe;  
     RETURN(existe);
 END $$
+
+-- FUNCIÓN PARA VERIFICAR SI YA EXISTE UNA SOLICITUD DE AMISTAD
+DROP FUNCTION IF EXISTS request_exists $$
+CREATE FUNCTION request_exists(
+	email_in VARCHAR(255),
+	friend_email_in VARCHAR(255)
+)
+RETURNS BOOLEAN
+DETERMINISTIC
+BEGIN
+	DECLARE existe BOOLEAN;
+	SELECT EXISTS (
+		SELECT 1
+		FROM Friends f 
+		WHERE f.email = email_in
+		AND f.friend_email = friend_email_in
+	) INTO existe;
+    RETURN(existe);
+END $$
+
+-- FUNCIÓN PARA VERIFICAR SI YA EXISTE UNA SOLICITUD DE AMISTAD
+DROP FUNCTION IF EXISTS request_received $$
+CREATE FUNCTION request_received(
+	email_in VARCHAR(255),
+	friend_email_in VARCHAR(255)
+)
+RETURNS BOOLEAN
+DETERMINISTIC
+BEGIN
+	DECLARE existe BOOLEAN;
+	SELECT EXISTS (
+		SELECT 1
+		FROM Friends f 
+		WHERE f.friend_email  = email_in
+		AND f.email  = friend_email_in
+	) INTO existe;
+    RETURN(existe);
+END $$
+
+
+-- Función para verificar que una amistad exista
+DROP FUNCTION IF EXISTS friendship_exists $$
+CREATE FUNCTION friendship_exists(
+	friendship_id INTEGER
+)
+RETURNS BOOLEAN
+DETERMINISTIC
+BEGIN
+	DECLARE existe BOOLEAN;
+    SELECT EXISTS( SELECT 1 FROM Friends f WHERE f.id_friendship = friendship_id) INTO existe;  
+    RETURN(existe);
+END $$
+
+
+-- Función para verificar que dos personas sean amigas
+DROP FUNCTION IF EXISTS friends $$
+CREATE FUNCTION friends(
+	email_in VARCHAR(255),
+	friend_email_in VARCHAR(255)
+)
+RETURNS BOOLEAN
+DETERMINISTIC
+BEGIN
+	DECLARE friends BOOLEAN;
+	SELECT EXISTS(
+		SELECT 1
+		FROM Friends f 
+		WHERE ((f.email = email_in AND f.friend_email = friend_email_in)
+		OR (f.email = friend_email_in AND f.friend_email = email_in))
+		AND (f.confirmed = TRUE)
+	) INTO friends;
+	RETURN(friends);
+END $$
+
+
+
+
+-- Función para obtener la fecha actual
+DROP FUNCTION IF EXISTS actual_date $$
+CREATE FUNCTION actual_date(
+)
+RETURNS DATETIME
+DETERMINISTIC
+BEGIN
+	DECLARE fecha DATETIME;
+    SELECT CONVERT_TZ(NOW(), 'SYSTEM', 'America/Guatemala') INTO fecha;
+    RETURN (fecha);
+END $$
+
+
+
